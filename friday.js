b34e628f0456ec9732b344fdd0a6c265c330c6ba
chemzqm <chemzqm@gmail.com>
2015-11-28 01:13:27 +0800

    Update documentation

---
+++ friday.js
@@ -0,0 +1,48 @@
+
+if (window.navigator.standalone) {
+  // stop stupid safari over scroll
+  document.addEventListener('touchmove', function(e) {
+    e.preventDefault()
+  })
+}
+
+screen.lockOrientationUniversal = screen.lockOrientation || screen.mozLockOrientation || screen.msLockOrientation;
+
+try {
+  if (screen.lockOrientationUniversal("landscape-primary")) {
+    // only works for Chrome for Android
+    // https://developer.mozilla.org/en-US/docs/Web/API/Screen/lockOrientation
+  }
+} catch(e) {
+}
+
+var Friday = require('..').friday
+
+var el = document.getElementById('main')
+var dateEl = document.getElementById('date')
+var f = new Friday(el, dateEl)
+
+// show time
+f.process.then(function() {
+  if (stopped) return Promise.reject()
+  return f.next()
+}).then(function() {
+  if (stopped) return Promise.reject()
+  return f.wait(200)
+}).then(function() {
+  if (stopped) return Promise.reject()
+  return f.prev()
+}).then(function() {
+  if (stopped) return Promise.reject()
+}).catch(function() {
+  return false
+})
+
+var stopped
+function stop() {
+  stopped = true
+}
+
+var container = document.querySelector('.container')
+container.addEventListener('touchstart', stop, false)
+container.addEventListener('mousedown', stop, false)
